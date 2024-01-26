a1a6a8e3fafa8174872341a060c70200264bcf8b
Yi Tam <tam.yi3d@gmail.com>
2024-01-26 14:16:44 +0100

    install and set up graphql-yoga

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,21 @@
 import {buildSchema} from "type-graphql";
 import {TestQueryResolver} from "@graphql/test/resolvers/query-resolver";
+import {createYoga} from "graphql-yoga";
+import { createServer } from 'node:http'
 
 async function bootstrap() {
   const schema = await buildSchema({
     resolvers: [ TestQueryResolver ],
+  })
+
+  // Create a Yoga instance with a GraphQL schema.
+  const yoga = createYoga({ schema })
+
+// Pass it into a server to hook into request handlers.
+  const server = createServer(yoga)
+
+// Start the server and you're done!
+  server.listen(4000, () => {
+    console.info('Server is running on http://localhost:4000/graphql')
   })
 }
