625def15a173d53c7f459f3ed508c600c5495730
Yi Tam <tam.yi3d@gmail.com>
2024-01-27 20:23:16 +0100

    feat: call query on the page from the server side

--- apps/web/src/lib/apollo/server-client.ts
+++ apps/web/src/lib/apollo/server-client.ts
@@ -2,6 +2,6 @@
 import { registerApolloClient } from '@apollo/experimental-nextjs-app-support/rsc'
 import {apolloClient} from "@/lib/apollo/client";
 
-export const {} = registerApolloClient(() => {
+export const { getClient } = registerApolloClient(() => {
   return apolloClient()
 })
