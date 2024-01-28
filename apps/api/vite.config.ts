86114e60903fcfef860226d0af3f1d728d0f6023
Yi Tam <tam.yi3d@gmail.com>
2024-01-28 15:10:12 +0100

    feat:  set up coverage

--- apps/api/vite.config.ts
+++ apps/api/vite.config.ts
@@ -2,10 +2,23 @@
 
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { configDefaults } from 'vitest/config'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-
+    reporters: ['html'],
+    coverage: {
+      reporter: ['html'],
+      reportsDirectory: '.coverage',
+      enabled: true,
+      provider: 'istanbul',
+      exclude: [
+        ...configDefaults.exclude,
+        "**/html/**",
+        "**/src/index.ts",
+        "**/test/utils/**",
+      ]
+    }
   }
 })
